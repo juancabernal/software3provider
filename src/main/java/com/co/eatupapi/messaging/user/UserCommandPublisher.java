ea63126e129a1deba82485c6892617a4942afddd
Alejo Arbelaez <alejoarbelaez@MacBook-Air-de-Alejo.local>
2026-05-09 08:36:55 -0500

    <correccion> Cambio de paquete de mensajería a messagi

--- src/main/java/com/co/eatupapi/messaging/user/UserCommandPublisher.java
+++ src/main/java/com/co/eatupapi/messaging/user/UserCommandPublisher.java
@@ -10,7 +10,7 @@ import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component; 
 
 import java.util.UUID;
 
